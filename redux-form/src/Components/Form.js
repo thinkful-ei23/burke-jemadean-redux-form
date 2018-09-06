8bbcc17e726fb5fbf6ee1884b29d9773cf3a9e4f
Jemadean Dobrosielski <jemadean@gmail.com>
2018-09-06 09:18:20 -0700

    created static version of form

---
+++ redux-form/src/Components/Form.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Form(props) {
+  return(
+    <form>
+      <div>
+        <label for="trackingNumber">Tracking number</label>
+        <input name="trackingNumber" id="trackingNumber"></input>
+      </div>
+      <div>
+        <label for="issue">What is your issue?</label>
+        <select name="issue" id="issue">
+          <option value="not-delivered">My delivery hasn't arrived</option>
+          <option value="wrong-item">The wrong item was delivered</option>
+          <option value="missing-part">Part of my order was missing</option>
+          <option value="damaged">Some of my order arrived damaged</option>
+          <option value="other">Other (give details below)</option>
+        </select>
+      </div>
+      <div>
+      <label for="details">Give more details (optional)</label>
+        <textarea name="details" id="details"></textarea>
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  )
+}
